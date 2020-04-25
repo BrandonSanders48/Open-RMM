95ffcbd4a799a36c05108c647a0827aac1925971
Brad Sanders <thebradleysanders@users.noreply.github.com>
2020-04-25 18:28:47 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,8 @@
 <?php
 	require("Includes/db.php");
-	$_SESSION['userid'] = "";
+	unset($_SESSION['userid']);
 	session_unset();
 	session_destroy();
 	header("Location: login.php");
-	exit("Redirecting");
-	if(!isset($_SESSION['userid'])){
-		http_response_code(404);
-		die();
-	}
+	exit("Redirecting");
 ?>Logging you out securely...
