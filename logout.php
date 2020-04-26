4fd79e2b2fbfa11a7f362282bb9e852362ebd44f
Brad Sanders <thebradleysanders@users.noreply.github.com>
2020-04-25 20:56:19 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,13 @@
 <?php
-	require("Includes/db.php");
+	require("Includes/db.php");
+
 	unset($_SESSION['userid']);
 	session_unset();
 	session_destroy();
 	sleep(1);
 	
 	if(isset($_SESSION['userid'])){
-		exit("We had trouble logging you out... it's our fault not yours.");
+		exit("We had trouble logging you out... It's our fault not yours.");
 	}else{
 		header("Location: login.php");
 		exit("Logging you out securely...");
